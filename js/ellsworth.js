9ea2a1534019488a5463019e5eed4f57a22efa57
Doug <thesecretaryofwar@gmail.com>
2014-08-11 17:28:04 -0400

    house cleaning

--- js/ellsworth.js
+++ js/ellsworth.js
@@ -10,8 +10,7 @@ var ec = {
   macros: {},
   replacements: {},
   environs: {},
-  biblio: "",
-  mathjax_scale: 1.0,
+  biblio: ""
 };
 
 function EllsworthConfig(c) {
@@ -28,35 +27,28 @@ function EllsworthConfig(c) {
   }
 
   MathJax.Hub.Config({
-    config: ["MMLorHTML.js"],
-    jax: ["input/TeX"],
-    "HTML-CSS": {
-      scale: ec["mathjax_scale"],
-      linebreaks: { automatic: true, width: "100%" }
-    },
-    SVG: {
-      scale: ec["mathjax_scale"],
-      linebreaks: { automatic: true, width: "100%" }
-    },
+    MMLorHTML: { prefer: { Firefox: "MML" } },
     tex2jax: {
       inlineMath: [ ['$','$'], ["\\(","\\)"] ],
       displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
     },
-    extensions: ["tex2jax.js"],
     TeX: {
-      extensions: ["AMSmath.js","AMSsymbols.js","noErrors.js","noUndefined.js"],
       Macros: ec["macros"]
     }
   });
+  MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 }
 
+// perform substitutions once document is ready
 jQuery(document).ready(function($) {
+  // perform string substitutions based on a dictionary, kind of a lame implementation of python format
   String.prototype.format_dict = function(dict) {
     return this.replace(/{(.+?)}/g, function(match,key) {
       return (typeof(dict[key])!='undefined') ? dict[key] : key;
     });
   };
 
+  // for a hover event and scale factor (of the realized object), generate appropriate css
   get_offset = function(event,scale) {
     if (!scale) { scale = 1.0; }
 
@@ -82,6 +74,7 @@ jQuery(document).ready(function($) {
             '-webkit-transform': 'translate('+trans_x+'%,'+trans_y+'%) scale('+scale+')'};
   };
 
+  // get a dictionary of attributes for an element, this doesn't exist already?
   get_attributes = function(elem) {
     var attributes = {};
     $(elem[0].attributes).each(function(index,attr) {
@@ -90,6 +83,7 @@ jQuery(document).ready(function($) {
     return attributes;
   };
 
+  // attach a popup to parent using given scale factor (default 1.0)
   attach_popup = function(parent,popup,scale) {
     parent.append(popup);
     parent.hover(function(event) {
@@ -101,8 +95,8 @@ jQuery(document).ready(function($) {
     });
   }
 
-  // create outer box
-  $("body").append($("<div>",{class:"outer_box"}).append($("body>")));
+  // create outer box for everyone to live in
+  $("body").append($("<div>",{class:"outer_box container",role:"main"}).append($("body>")));
   outer_box = $("div.outer_box");
 
   // make header - title, author, abstract
@@ -138,25 +132,29 @@ jQuery(document).ready(function($) {
     return div;
   });
 
-  // do replacements
+  // insert section titles
   var n_sections = 0;
   $("section").replaceWith(function () {
     var sec = $(this);
-    var div = $("<div>",{class:"section_box",sec_num:++n_sections,sec_title:sec.attr("title"),label:sec.attr("label"),n_subsections:0});
+    var label = sec.attr("label");
+    var div = $("<div>",{class:"section_box",sec_num:++n_sections,sec_title:sec.attr("title"),n_subsections:0});
+    if (label) { div.attr("id","section_"+label); }
     div.append($("<h2>",{html:div.attr("sec_num")+" &nbsp; "+div.attr("sec_title"),class:"section_title"}));
     div.append(sec.children());
     return div;
   });
 
-  // one per section - this is bad
+  // we only go down to the subsection now - this is bad
   $("subsection").replaceWith(function () {
     var sec = $(this);
+    var label = sec.attr("label");
     parent = sec.parent("div.section_box");
     sec_num = parent.attr("sec_num");
     subsec_num = Number(parent.attr("n_subsections"))+1;
     parent.attr("n_subsections",subsec_num);
     num_text = sec_num+"."+subsec_num;
-    var div = $("<div>",{class:"subsection_box",sec_title:sec.attr("title"),label:sec.attr("label"),sec_num:sec_num,subsec_num:subsec_num});
+    var div = $("<div>",{class:"subsection_box",sec_title:sec.attr("title"),sec_num:sec_num,subsec_num:subsec_num});
+    if (label) { div.attr("id","subsec_"+label); }
     div.append($("<h3>",{html:sec_num+"."+subsec_num+" &nbsp; "+sec.attr("title"),class:"subsection_title"}));
     div.append(sec.children());
     return div;
@@ -176,45 +174,53 @@ jQuery(document).ready(function($) {
   var n_environs;
   for (env in ec["environs"]) {
     n_environs = 0;
+    rule = ec["environs"][env];
+    content = rule[0];
     $(env).replaceWith(function () {
       elem = $(this);
-      rule = ec["environs"][env];
-      content = rule[0];
+      label = elem.attr("label");
       orig_html = elem.html();
       elem.attr("number",++n_environs);
       elem.attr("content",orig_html);
-      div = $("<div>",{class:env+"_box",html:content.format_dict(get_attributes(elem)),number:n_environs,label:elem.attr("label"),content:orig_html});
+      div = $("<div>",{class:env+"_box",html:content.format_dict(get_attributes(elem)),number:n_environs,content:orig_html});
+      if (label) { div.attr("id",env+"_"+label); }
       return div;
     })
   }
 
+  // basic text environment
   $("text").replaceWith(function () {
     var text = $(this).html();
     var p = $("<p>",{class:"paragraph_box",html:text});
     return p;
   });
 
+  // create footnotes and attach hover popups
   var n_footnotes = 0;
   $("footnote").replaceWith(function () {
     var foot = $(this);
     var foot_num = ++n_footnotes;
     var foot_text = foot.html();
+    var span = $("<span>")
     var sup = $("<sup>",{class:"footnote_mark",foot_num:foot_num,html:foot_num,label:foot.attr("label"),foot_text:foot_text});
-    var popup = $("<div>",{class:"footnote_popup",html:foot_text});
-    attach_popup(sup,popup);
-    return sup;
+    span.append(sup);
+    var popup = $("<div>",{class:"popup footnote_popup",html:foot_text});
+    attach_popup(span,popup);
+    return span;
   });
 
+  // generate equations - this wraps everything in an align environment, rather than the MathJax builtin $$, which maps to equation I assume
   var n_equations = 0;
   $("equation").replaceWith(function () {
     var eqn = $(this);
-    var div_box = $("<div>",{class:"equation_box"});
+    var label = eqn.attr("label");
+    var div_box = $("<div>",{class:"equation_box container-fluid"});
     var div = $("<div>",{class:"equation_inner",html:"\n\\begin{align*}"+$(this).html()+"\n\\end{align*}"});
     div_box.append(div);
-    if (eqn.attr("label")) {
+    if (label) {
+      div_box.attr("id","equation_"+label);
       var eqn_num = ++n_equations;
-      div.attr("label",eqn.attr("label"));
-      div.attr("eqn_num",eqn_num);
+      div_box.attr("eqn_num",eqn_num);
       var opp_div = $("<div>",{class:"equation_number"});
       div.before(opp_div);
       var num_div = $("<div>",{class:"equation_number",html:eqn_num});
@@ -223,16 +229,19 @@ jQuery(document).ready(function($) {
     return div_box;
   });
 
-  // image is taken for whatever reason
+  // standard image container - uses bootstrap responsive resizing
   $("media").replaceWith(function () {
-    return $("<img>",{src:$(this).attr("source"),class:"media"});
+    return $("<img>",{src:$(this).attr("source"),class:"media img-responsive"});
   });
 
+  // figures - numbering for whole document only
   var n_figures = 0;
   $("figure").replaceWith(function () {
     var fig = $(this);
+    var label = fig.attr("label");
     var fig_num = ++n_figures;
-    var div = $("<div>",{class:"figure_box",fig_num:fig_num,label:fig.attr("label")});
+    var div = $("<div>",{class:"figure_box",fig_num:fig_num});
+    if (label) { div.attr("id","figure_"+label); }
     if (fig.attr("title")) {
       div.append($("<h3>",{html:"Figure "+fig_num+": "+fig.attr("title"),class:"figure_title"}));
     }
@@ -243,11 +252,14 @@ jQuery(document).ready(function($) {
     return div;
   });
 
+  // tables - whole document numbering
   var n_tables = 0;
   $("table").replaceWith(function () {
     var tab = $(this);
+    var label = tab.attr("label")
     var tab_num = ++n_tables;
-    var div = $("<div>",{class:"table_box",tab_num:tab_num,label:tab.attr("label")});
+    var div = $("<div>",{class:"table_box",tab_num:tab_num});
+    if (label) { div.attr("id","table_"+label); }
     if (tab.attr("title")) {
       div.append($("<h3>",{html:"Table "+tab_num+": "+tab.attr("title"),class:"table_title"}));
     }
@@ -267,6 +279,7 @@ jQuery(document).ready(function($) {
     tbody.addClass("df_tbody");
   });
 
+  // ordered lists - autonumbering!
   $("enumerate").replaceWith(function () {
     var enumer = $(this);
     var div = $("<div>",{class:"enumerate_box",n_items:0});
@@ -283,33 +296,38 @@ jQuery(document).ready(function($) {
     return div;
   });
 
-  // dereference refs
+  // dereference refs - attach appropriate hover popups
   $("ref").replaceWith(function () {
     var ref = $(this);
     var span = $("<span>",{class:"ref_text"});
-    if (ref.attr("label")) {
-      var label = ref.attr("label");
-      if ((fig=$("div.figure_box[label="+label+"]")).length) {
-        span.html("Figure "+fig.attr("fig_num"));
-        var popup = $("<div>",{class:"fig_popup",html:fig.html()});
+    if (label=ref.attr("label")) {
+      if ((fig=$("div.figure_box[id=figure_"+label+"]")).length) {
+        link = $("<a>",{class:"ref_link fig_link",href:"#figure_"+label,html:"Figure "+fig.attr("fig_num")});
+        span.append(link);
+        var popup = $("<div>",{class:"popup fig_popup",html:fig.html()});
         attach_popup(span,popup,0.5);
-      } else if ((tab=$("div.table_box[label="+label+"]")).length) {
-        span.html("Table "+tab.attr("tab_num"));
-        var popup = $("<div>",{class:"tab_popup",html:tab.html()});
+      } else if ((tab=$("div.table_box[id=table_"+label+"]")).length) {
+        link = $("<a>",{class:"ref_link tab_link",href:"#table_"+label,html:"Table "+tab.attr("tab_num")});
+        span.append(link);
+        var popup = $("<div>",{class:"popup tab_popup",html:tab.html()});
         attach_popup(span,popup);
-      }else if ((eqn=$("div.equation_inner[label="+label+"]")).length) {
-        span.html("Equation "+eqn.attr("eqn_num"));
-        var popup = $("<div>",{class:"eqn_popup",html:eqn.html()});
+      } else if ((eqn=$("div.equation_box[id=equation_"+label+"]")).length) {
+        link = $("<a>",{class:"ref_link eqn_link",href:"#equation_"+label,html:"Equation "+eqn.attr("eqn_num")});
+        span.append(link);
+        var popup = $("<div>",{class:"popup eqn_popup",html:eqn.children(".equation_inner").html()});
         attach_popup(span,popup);
-      } else if ((sec=$("div.section_box[label="+label+"]")).length) {
-        span.html("Section "+sec.attr("sec_num"));
-        var popup = $("<div>",{class:"sec_popup",html:sec.attr("sec_title")});
+      } else if ((sec=$("div.section_box[id=section_"+label+"]")).length) {
+        link = $("<a>",{class:"ref_link sec_link",href:"#section_"+label,html:"Section "+sec.attr("sec_num")});
+        span.append(link);
+        var popup = $("<div>",{class:"popup sec_popup",html:sec.attr("sec_title")});
         attach_popup(span,popup);
-      } else if ((sec=$("div.subsection_box[label="+label+"]")).length) {
-        span.html("Section "+sec.attr("sec_num")+'.'+sec.attr("subsec_num"));
-        var popup = $("<div>",{class:"sec_popup",html:sec.attr("sec_title")});
+      } else if ((sec=$("div.subsection_box[id=subsec_"+label+"]")).length) {
+        link = $("<a>",{class:"ref_link subsec_link",href:"#subsec_"+label,html:"Section "+sec.attr("sec_num")+'.'+sec.attr("subsec_num")});
+        span.append(link);
+        var popup = $("<div>",{class:"popup sec_popup",html:sec.attr("sec_title")});
         attach_popup(span,popup);
       } else {
+        // look for matches in the custom environments
         found = false;
         for (env in ec["environs"]) {
           rule = ec["environs"][env];
@@ -320,10 +338,11 @@ jQuery(document).ready(function($) {
             } else {
               tip = '';
             }
-            if ((div=$("div."+env+"_box[label="+label+"]")).length) {
+            if ((div=$("div."+env+"_box[id="+env+"_"+label+"]")).length) {
               attrib = get_attributes(div);
-              attrib['html'] = div.html();
-              span.html(reference.format_dict(attrib));
+              attrib["html"] = div.html();
+              link = $("<a>",{class:"ref_link "+env+"_link",href:"#"+env+"_"+label,html:reference.format_dict(attrib)});
+              span.append(link);
               if (tip.length) {
                 var popup = $("<div>",{class:"popup "+env+"_popup",html:tip.format_dict(attrib)});
                 attach_popup(span,popup);
@@ -334,15 +353,18 @@ jQuery(document).ready(function($) {
           }
         }
         if (!found) {
-          span.html("label:"+label);
+          // nothing doing - just output the label in error
+          span.html("label: "+label).css({"color":"red"});
         }
       }
     }
     return span;
   });
 
+  // on biblio load - parse, in text cites, end list
   if (ec["biblio"]) {
     $.getJSON(ec["biblio"],function(sources) {
+      // parse bibliography into usable structure - a real shit show
       for (label in sources) {
         // collect author names
         var source = sources[label];
@@ -402,32 +424,36 @@ jQuery(document).ready(function($) {
         if (("volume" in source)&&("number" in source)&&("pages" in source)) {
           bib_form += source["volume"]+" ("+source["number"]+"): "+source["pages"]+".";
         }
-        source["bib_form"] = bib_form;
+        source["biblio_form"] = bib_form;
 
         // track if we should put it in bibliography
         source["used"] = false;
       }
 
+      // check for end list
+      has_biblio = $("bibliography").length > 0;
+
       // dereferences citations
       $("cite").replaceWith(function () {
         var cite = $(this);
-        var span = $("<span>",{class:"cite_text"});
+        span = $("<span>");
         if (cite.attr("label")) {
           var label = cite.attr("label");
           if (sources.hasOwnProperty(label)) {
             src = sources[label];
             src["used"] = true;
-            //link_url = "http://scholar.google.com/scholar?q="+encodeURIComponent(src["title"]);
-            //var link = $("<a>",{href:link_url,html:src["cite_form"]})
-            span.html(src["cite_form"]);
+            link = $("<a>",{class:"cite_link"});
+            if (has_biblio) { link.attr("href","#biblio_"+label); }
+            link.html(src["cite_form"]);
+            span.append(link);
             pop_txt = src["title"];
             if (src.hasOwnProperty("journal")) {
               pop_txt += " ("+src["journal"]+")";
             }
-            popup = $("<div>",{html:pop_txt,class:"cite_popup"});
+            popup = $("<div>",{html:pop_txt,class:"popup cite_popup"});
             attach_popup(span,popup);
           } else {
-            span.html("source:"+label);
+            span.html("source:"+label).css({"color":"red"});
           }
         }
         return span;
@@ -442,13 +468,14 @@ jQuery(document).ready(function($) {
         for (label in sources) {
           var source = sources[label];
           if (source["used"]) {
-            ref_list.push(source["bib_form"]);
+            ref_list.push({"biblio_html":source["biblio_form"],"biblio_label":label});
           }
         }
-        ref_list.sort();
+        ref_list.sort(function (a,b) { return a["biblio_html"] > b["biblio_html"]; });
         for (i in ref_list) {
           var ref = ref_list[i];
-          bib.append($("<div>",{html:ref,class:"bibliography_item"}));
+          bib.append($("<a>",{name:"biblio_"+ref["biblio_label"]}));
+          bib.append($("<div>",{html:ref["biblio_html"],class:"bibliography_item"}));
         }
         return bib;
       });
@@ -459,7 +486,7 @@ jQuery(document).ready(function($) {
       var span = $("<span>",{class:"cite_text"});
       if (cite.attr("label")) {
         var label = cite.attr("label");
-        span.html("source:"+label);
+        span.html("source:"+label).css({"color":"red"});
       }
       return span;
     });
